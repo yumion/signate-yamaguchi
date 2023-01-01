4ba2d6a71cf35e587f107ae1264eccab77a6d93e
yumion <a.yamada0703@gmail.com>
2023-01-01 16:08:56 +0900

    refactoring

--- src/divide_dataset.py
+++ src/divide_dataset.py
@@ -9,13 +9,14 @@ from convert_coco import load_annotation, save_annotation
 
 def main():
     src_dir = Path('../input/train_image')
-    dst_dir = Path('../input/train_5cv')
+    dst_dir = Path('../input/train_5cv_3classes')
+    annotation_filename = 'annotations_3classes.json'
     n_splits = 5
 
     data = get_dataframe(src_dir)
     train, test = data_split(data, n_splits=n_splits)
-    save_data(test, src_dir, dst_dir, train_test='test', symlink=True)  # foldフォルダ直下に画像を全て保存
-    save_data(train, src_dir, dst_dir, train_test='train', symlink=True)  # foldフォルダ直下に画像を全て保存
+    save_cv(test, src_dir, dst_dir, annotation_filename, train_test='test', symlink=True)  # foldフォルダ直下に画像を全て保存
+    save_cv(train, src_dir, dst_dir, annotation_filename, train_test='train', symlink=True)  # foldフォルダ直下に画像を全て保存
     # save_group(test, src_dir, dst_dir, symlink=True)  # 動画単位でフォルダ分けして保存
 
 
@@ -48,6 +49,34 @@ def data_split(data, n_splits):
     return train, test
 
 
+def save_cv(folds: dict, src_dir: Path, dst_dir: Path, annotation_filename: str, train_test: str = 'test', symlink: bool = False):
+    for i, fold in folds.items():
+        save_image(fold, src_dir, dst_dir / f'cv{i + 1}/{train_test}', symlink)
+        dst_json = concat_annotation(src_dir, list(set(fold['group'])), annotation_filename)
+        save_annotation(dst_json, dst_dir / f'cv{i + 1}/{train_test}' / annotation_filename)
+
+
+def save_image(fold: dict, src_dir: Path, dst_dir: Path, symlink: bool = False):
+    for video, image in tqdm(zip(fold['group'], fold['data']), desc='image'):
+        src_image_p = src_dir / video / 'images' / image
+        dst_image_p = dst_dir / 'images' / image
+        dst_image_p.parent.mkdir(parents=True, exist_ok=True)
+        if symlink:
+            dst_image_p.resolve().symlink_to(src_image_p.resolve())
+        else:
+            shutil.copytree(src_image_p, dst_image_p)
+
+
+def concat_annotation(src_dir: Path, sub_dir_names: list, filename: str):
+    dst_json = {'images': [], 'annotations': []}
+    for video in tqdm(sub_dir_names, desc='annotation'):
+        src_json = load_annotation(src_dir / video / filename)
+        dst_json['images'].extend(src_json['images'])
+        dst_json['annotations'].extend(src_json['annotations'])
+        dst_json['categories'] = src_json['categories']
+    return dst_json
+
+
 def save_group(folds, src_dir, dst_dir, symlink=False):
     for i, fold in folds.items():
         groups = np.unique(fold['group'])
@@ -59,28 +88,6 @@ def save_group(folds, src_dir, dst_dir, symlink=False):
                 symlink_dir(src_p, dst_p)
             else:
                 shutil.copytree(src_p, dst_p)
-
-
-def save_data(folds, src_dir, dst_dir, train_test='test', symlink=False):
-    for i, fold in folds.items():
-        for video, image in tqdm(zip(fold['group'], fold['data']), desc=f'image:cv{i + 1}/{train_test}'):
-            # images
-            src_image_p = src_dir / video / 'images' / image
-            dst_image_p = dst_dir / f'cv{i + 1}/{train_test}' / 'images' / image
-            dst_image_p.parent.mkdir(parents=True, exist_ok=True)
-            if symlink:
-                dst_image_p.resolve().symlink_to(src_image_p.resolve())
-            else:
-                shutil.copytree(src_image_p, dst_image_p)
-
-        # annotation json
-        dst_json = {'images': [], 'annotations': []}
-        for video in tqdm(list(set(fold['group'])), desc=f'annotation:cv{i + 1}/{train_test}'):
-            src_json = load_annotation(src_dir / video / 'annotations.json')
-            dst_json['images'].extend(src_json['images'])
-            dst_json['annotations'].extend(src_json['annotations'])
-            dst_json['categories'] = src_json['categories']
-        save_annotation(dst_json, dst_dir / f'cv{i + 1}/{train_test}' / 'annotations.json')
 
 
 def symlink_dir(src_dir, dst_dir):
