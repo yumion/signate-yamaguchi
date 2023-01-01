82790cbc1671b0cd98f6ae5153161585a9cdcf9c
yumion <a.yamada0703@gmail.com>
2023-01-01 18:05:35 +0900

    rendering predition

---
+++ src/make_prediction_video.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from tqdm import tqdm
+from PIL import Image
+import numpy as np
+import imageio
+
+
+def main():
+    result_dir = Path('../work_dirs/retinanet_r50_fpn_1x_coco_3classes/20230101_083452/work_dirs/retinanet_r50_fpn_1x_coco_3classes')
+    save_dir = result_dir / 'video'
+
+    save_dir.mkdir(parents=True, exist_ok=True)
+    pre_scene = None
+    for img_p in tqdm(result_dir.glob('*.png')):
+        scene = '_'.join(img_p.name.split('_')[:-1])
+        if pre_scene is None:
+            # 新しい動画ならwriterを新しく立てる
+            tqdm.write(f'first video: {scene}')
+            writer = VideoWriter(save_dir / f'{scene}.mp4')
+        elif pre_scene != scene:
+            # 次の動画ならwriterを立て直す
+            writer.release()
+            tqdm.write(f'next video: {scene}')
+            writer = VideoWriter(save_dir / f'{scene}.mp4')
+        else:
+            # 同じ動画ならフレームを追加していく
+            image = np.array(Image.open(img_p))
+            writer.write(image)
+        pre_scene = scene
+
+
+class VideoWriter:
+    def __init__(self, video_path, fps=10):
+        self.writer = imageio.get_writer(
+            video_path, format='ffmpeg', fps=fps)
+
+    def release(self):
+        self.writer.close()
+
+    def write(self, image):
+        self.writer.append_data(image)
+
+
+if __name__ == '__main__':
+    main()
